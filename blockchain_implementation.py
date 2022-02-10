b20af3cde8825b73b7ffe37cbf1622e5a6b0090f
Abdurrahman Eshibiny <a.eshibiny@gmail.com>
2022-02-10 19:01:03 +0200

    added fn to print chain

--- blockchain_implementation.py
+++ blockchain_implementation.py
@@ -5,24 +5,26 @@ import hashlib
 import json
 import timeit
 
-class block:
+from numpy import block
+
+class Block:
 	def __init__(self, index, proof, previous_hash):
 		self.children = []
 		self.data = {
 			'index': index,
 			'timestamp': str(datetime.datetime.now()),
-			'transaction': self.generate_rand_trans(),
+			'transaction': ''.join(random.choices(string.ascii_lowercase, k=50)),
 			'proof': proof,
 			'previous hash': previous_hash
 		}
 
-	def generate_rand_trans():
-		return ''.join(random.choices(string.ascii_lowercase, k=50))
+	# def generate_rand_trans(self):
+	# 	return ''.join(random.choices(string.ascii_lowercase, k=50))
 
-class blockchain:
+class Blockchain:
 	def __init__(self):
 		# Initialize genesis block
-		self.first_block = block(1, 1, '0')
+		self.first_block = Block(1, 1, '0')
 		self.length = 1
 		self.last_block = self.first_block
 
@@ -31,10 +33,11 @@ class blockchain:
 		self.num_zeros = self.calc_zeros()
 		print("Determined complexity is", self.num_zeros, "zeros for successful hash mine")
 	
+
 	# Functions that help initialize the num_zeros variable that controls complexity
 	def proof_of_work_test(self, N):
 		proof = 0
-		block = {
+		dummy_block = {
 			'index': 9,
 			'timestamp': str(datetime.datetime.now()),
 			'transactions': ''.join(random.choices(string.ascii_lowercase, k=50)),
@@ -44,15 +47,14 @@ class blockchain:
 		check_proof = False
 		while check_proof is False:
 			hash_operation = hashlib.sha256(
-				str(json.dumps(block, sort_keys=True)).encode()).hexdigest()
+				str(json.dumps(dummy_block, sort_keys=True)).encode()).hexdigest()
 			
 			if hash_operation[:N] == '0'*N:
 				check_proof = True
 			else:
-				proof += 1
-				block['proof']+=1
+				# proof += 1
+				dummy_block['proof']+=1
 
-		return block
 
 	def calc_zeros(self):
 		for i in range(1,256):
@@ -68,18 +70,21 @@ class blockchain:
 
 	def add_block(self):
 		self.last_block.children.append(
-			block(
+			Block(
 				self.last_block.data['index'] + 1,
 				0,
-				self.hash(self.last_data.data)))
+				self.hash(self.last_block)))
 		self.proof_of_work(self.last_block.children[0])
 		self.last_block = self.last_block.children[0]
+		self.length+=1
 	
+
 	def hash(self, block):
 		# Block argument is the block-class instance we need to hash
 
 		encoded_block = json.dumps(block.data, sort_keys=True).encode()
 		return hashlib.sha256(encoded_block).hexdigest()
+
 
 	def proof_of_work(self, block):
 		# Block argument is the block-class instance we need to mine
@@ -92,3 +97,31 @@ class blockchain:
 				check_proof = True
 			else:
 				block.data['proof'] += 1
+
+
+	def print_chain(self, block):
+		output = []
+		output.append(block.data)
+		if(len(block.children) == 0):
+			return output
+		elif(len(block.children) == 1):
+			output += self.print_chain(block.children[0])
+			return output
+		# else:
+		# 	branches = {}
+		# 	for i in range(len(block.children)):
+				
+
+
+
+if __name__ == '__main__':
+	bc = Blockchain()
+	# start = timeit.default_timer()
+	bc.add_block()
+	bc.add_block()
+	bc.add_block()
+	bc.add_block()
+	# stop = timeit.default_timer()
+	# print(json.dumps(bc.first_block.children[0].data, indent=4))
+	# print(str(stop - start))
+	print(json.dumps({'Blockchain': bc.print_chain(bc.first_block)}, indent=4))
